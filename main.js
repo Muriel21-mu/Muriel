6b0864edc4ce52533d6bd837725bb2974d2bed3c
Edzisna Muriel <edzisnam@gmail.com>
2024-08-06 06:41:07 -0700

    changes

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
 let myVariable;
-myVariable ( "Bob");
+myVariable ("Bob");
+
+var x = 10;
+x += 5;
+document.getElementById("demo").innerHTML = x;
+
+let iceCream = "chocolate";
+if (iceCream === "chocolate") {
+  alert("Yay, I love chocolate ice cream!");
+} else {
+  alert("Awwww, but chocolate is my favorite…");
+}
